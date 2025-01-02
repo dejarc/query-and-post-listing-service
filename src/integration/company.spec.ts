c416c64be13a3ed99a693453e476c9ef30ce75eb
Christopher DeJarlais <cdejarc@gmail.com>
2025-01-02 12:53:07 -0700

    Add unit test for getAllCompaniesById

--- src/integration/company.spec.ts
+++ src/integration/company.spec.ts
@@ -1,5 +1,7 @@
-import { getCompanies, getCompanyById } from './company';
+import { getCompanies, getCompanyById, getAllCompaniesById } from './company';
 import { CompanyDB } from '../mock-services/company.db';
+import loki from 'lokijs';
+import { Company } from '../mock-services/company.model';
 describe('db-integration', () => {
   const mockData = [
     { id: '1', name: 'ACCELERATE SHIPPING' },
@@ -10,6 +12,11 @@ describe('db-integration', () => {
     { id: '6', name: 'EXPERT SHIPPING' },
     { id: '7', name: 'EXPRESS SHIPPING' },
   ];
+  const db = new loki('companies.db');
+  const companies = db.addCollection<Company>('companies');
+  mockData.forEach((company) => {
+    companies.insert(company);
+  });
   afterAll(() => {
     jest.restoreAllMocks();
   });
@@ -18,22 +25,39 @@ describe('db-integration', () => {
       jest
         .spyOn(CompanyDB.prototype, 'getCompanyCompanyCollection')
         .mockImplementation(() => {
-          return mockData as any;
+          return companies;
         });
       const res = getCompanies();
-      expect(res).toEqual(mockData);
+      expect(res).toEqual(companies);
     });
   });
   describe('getCompanyById', () => {
     it('should return a single company', () => {
-      const mockEntry = mockData[0];
+      const id = '1';
+      const mockEntry = companies.findOne({ id });
       jest
         .spyOn(CompanyDB.prototype, 'getCompanyById')
-        .mockImplementation(() => {
-          return mockEntry as any;
+        .mockImplementation((id: string) => {
+          return companies.findOne({ id }) as Company;
         });
-      const res = getCompanyById('1');
+      const res = getCompanyById(id);
       expect(res).toEqual(mockEntry);
+    });
+  });
+  describe('getAllCompaniesById', () => {
+    it('should return a map of company ids to names', () => {
+      const mockData = {
+        '2': 'BARTER SHIPPING',
+        '3': 'BLINK SHIPPING',
+        '4': 'COMMAND SHIPPING',
+      };
+      jest
+        .spyOn(CompanyDB.prototype, 'getCompanyCompanyCollection')
+        .mockImplementation(() => {
+          return companies;
+        });
+      const res = getAllCompaniesById(['2', '3', '4']);
+      expect(res).toEqual(mockData);
     });
   });
 });
